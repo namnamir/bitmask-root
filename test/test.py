fb8b060d30534e27350d41000e2334185b95db1b
Alireza <alireza@iransec.org>
2015-06-18 13:17:17 +0430

    Add firewall implementation

---
+++ test/test.py
@@ -0,0 +1,20 @@
+import src.leap.bitmask.windows.firewall
+import unittest
+from src.leap.bitmask.windows.firewall import Firewall
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_get_firewall_status(self):
+        firewall = Firewall()
+        self.assertIn(firewall.get_firewall_status(), [True, False])
+
+    def test_firewall_start(self):
+        firewall = Firewall()
+        self.assertAlmostEqual(firewall.start(), True)
+
+    def test_firewall_stop(self):
+        firewall = Firewall()
+        self.assertAlmostEqual(firewall.stop(), False)
+
+if __name__ == '__main__':
+    unittest.main()
