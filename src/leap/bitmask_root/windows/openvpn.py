792b12e03e99d46e0509570251ab54db47828b8b
Alireza <alireza@iransec.org>
2015-08-02 23:26:57 +0430

    Update openvpn.py

--- src/leap/bitmask_root/windows/openvpn.py
+++ src/leap/bitmask_root/windows/openvpn.py
@@ -1,45 +1,56 @@
-import os
+import string
 import subprocess
-import time
 from tools import *
-
-
-class VPNLauncherException(Exception):
-    pass
-
-
-class OpenVPNNotFoundException(VPNLauncherException):
-    pass
+from management import OVPNInterface
 
 
 class OpenVPNLauncher:
     def __init__(self):
         self.proc = None
+        self.interface = OVPNInterface()
 
     def launch(self, cfgfile, logfile):
         try:
-            path = os.path.join(tools.get_bitmask_home(),'third-party','openvpn')
-            args = [os.path.join(path,'openvpn.exe'),
-                    "--config",os.path.join(path,cfgfile),
-                    "--log",os.path.join(path,logfile),
-                    "--management","127.0.0.1", "7505"]
+            path = os.path.join(Tools.get_bitmask_home(), 'third-party', 'openvpn')
+            args = [os.path.join(path, 'openvpn.exe'),
+                    "--config", os.path.join(path, cfgfile),
+                    "--log", os.path.join(path, logfile),
+                    "--management", "127.0.0.1", "7505"]
 
             self.proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
             if self.proc.returncode:
-                raise VPNLauncherException(
+                raise NotImplementedError(
                     "Executing external OpenVPN process failed returning %s" % self.proc.returncode)
-
             return self.proc.pid
 
         except OSError, e:
-            raise OpenVPNNotFoundException()
+            raise NotImplementedError("OpenVPN not found")
 
     def terminate(self):
-        try:
-            procs = tools.is_openvpn_running()
-            if procs:
-                for p in procs:
-                    p.terminate()
-        except:
-            pass
+        procs = Tools.is_openvpn_running()
+        if procs:
+            for p in procs:
+                p.terminate()
+
+    def get_status(self):
+        msg = self.interface.send("state\n")
+
+        if msg is not None:
+            lines = string.split(msg, os.linesep)
+            for l in lines:
+                if ',' in l:
+                    parts = string.split(l, ",")
+                    if len(parts) < 5:
+                        continue
+                    tstamp, state, desc, tun_ip, remote_ip = parts
+                    if state == "CONNECTED" and desc == "SUCCESS":
+                        return {'ovpn_state': "CONNECTED", 'interface': tun_ip.split('\r')[0], 'gateway': remote_ip}
+                    elif state == "CONNECTED" and desc == "ERROR":
+                        return {'ovpn_state': "DISCONNECTED"}
+                    else:
+                        return {'ovpn_state': state}
+                else:
+                    continue
+        else:
+            return {'ovpn_state': "DISCONNECTED"}
