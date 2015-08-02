fe16727e6682da631d77cc7031942eb5bda91343
Alireza <alireza@iransec.org>
2015-08-02 23:25:23 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from py2exe.build_exe import py2exe
 from distutils.core import setup
-import os
-import zmq.libzmq
+import sys
+
+sys.argv.append("py2exe")
 
 opts = {'py2exe': {
     'dist_dir': "dist",
+    'bundle_files': 1,
     'includes': ['zmq.backend.cython', 'UserList', 'UserString', 'commands', 'future.backports.misc'],
 }, }
 
-setup(service=['windows_installer'], options=opts, cmdline_style='pywin32')
+setup(service=['windows_installer'], options=opts, cmdline_style='pywin32',zipfile=None)
