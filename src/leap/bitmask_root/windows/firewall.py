7af48421163bde597956f180167e72c9c4f81e4e
Alireza <alireza@iransec.org>
2015-08-02 23:31:01 +0430

    Update setup.py

--- src/leap/bitmask_root/windows/firewall.py
+++ src/leap/bitmask_root/windows/firewall.py
@@ -5,19 +5,22 @@ class Firewall:
     def __init__(self):
         pass
 
-    def stop(self):
-        subprocess.call(['netsh', 'advfirewall', 'set', 'allprofiles', 'state', 'off'])
+    def stop(self, getway="192.168.1.1"):
+        subprocess.call(['route', 'add', '0.0.0.0', 'mask', '0.0.0.0', getway])
 
-    def start(self):
-        subprocess.call(['netsh', 'advfirewall', 'set', 'allprofiles', 'state', 'on'])
-
-    def get_firewall_status(self):
-        cmd = subprocess.Popen(['netsh', 'advfirewall', 'show', 'currentprofile'], stdout=subprocess.PIPE)
+    def parse_getway(self):
+        cmd = subprocess.Popen(['route', 'print'], stdout=subprocess.PIPE)
         out = cmd.stdout.readlines()
+        route_table = False
 
         for line in out:
-            if line.startswith('State'):
-                if line.split()[1] == "ON":
-                    return True
-                else:
-                    return False
+            if line.startswith('IPv4 Route Table'):
+                route_table = True
+            if route_table:
+                if line.split()[0] == "0.0.0.0" and line.split()[1] == "0.0.0.0":
+                    return line.split()[2]
+
+    def start(self):
+        getway = self.parse_getway()
+        subprocess.call(['route', 'delete', '0.0.0.0', 'mask', '0.0.0.0'])
+        return getway
