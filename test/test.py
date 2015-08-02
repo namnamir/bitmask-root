b3f37c6715435155a192ed6bdaea9d68e404b32e
Alireza <alireza@iransec.org>
2015-08-02 23:35:02 +0430

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,9 +1,7 @@
+from time import sleep, time
 import unittest
 from jsonrpc2_zeromq import RPCClient
-from os.path import abspath, dirname
-from src.leap.bitmask_root.windows.firewall import *
 from src.leap.bitmask_root.windows.windows_implementation import *
-from src.leap.bitmask_root.windows.tools import tools
 from src.leap.bitmask_root.windows.openvpn import *
 
 
@@ -11,25 +9,24 @@ class TestStringMethods(unittest.TestCase):
     def test_firewall_start(self):
         client = RPCClient("tcp://%s:%s" % (host, port))
         client.start_firewall()
-        firewall = Firewall()
-        self.assertAlmostEqual(firewall.get_firewall_status(), True)
 
     def test_firewall_stop(self):
         client = RPCClient("tcp://%s:%s" % (host, port))
-        client.stop_firewall()
-        firewall = Firewall()
-        self.assertAlmostEqual(firewall.get_firewall_status(), False)
+        client.stop_firewall("192.168.1.1")
 
     def test_start_ovpn(self):
         client = RPCClient("tcp://%s:%s" % (host, port))
-        self.proc = psutil.Process(client.start_ovpn('sample.ovpn', 'log.txt'))
-        self.assertAlmostEqual(self.proc.is_running(), True)
+        client.start_ovpn('sample.ovpn', 'log.txt')
+        ovpn = OpenVPNLauncher()
+        while ovpn.get_status().get('ovpn_state') != "CONNECTED":
+            print ovpn.get_status()
+            sleep(1)
+        client.start_firewall()
 
     def test_stop_ovpn(self):
         client = RPCClient("tcp://%s:%s" % (host, port))
         client.stop_ovpn()
-        self.assertAlmostEqual(len(tools.is_openvpn_running()), 0)
-        pass
+
 
 
 if __name__ == '__main__':
