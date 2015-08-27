535d8ccb3b64d9f93b6b200acc8513bc6a1c93e6
Alireza <alireza@iransec.org>
2015-08-27 15:24:36 +0430

    Update firewall.py

--- src/leap/bitmask_root/windows/firewall.py
+++ src/leap/bitmask_root/windows/firewall.py
@@ -1,26 +1,15 @@
-import subprocess
+from src.leap.bitmask_root.windows.route import Route
 
 
 class Firewall:
     def __init__(self):
         pass
 
-    def stop(self, getway="192.168.1.1"):
-        subprocess.call(['route', 'add', '0.0.0.0', 'mask', '0.0.0.0', getway])
-
-    def parse_getway(self):
-        cmd = subprocess.Popen(['route', 'print'], stdout=subprocess.PIPE)
-        out = cmd.stdout.readlines()
-        route_table = False
-
-        for line in out:
-            if line.startswith('IPv4 Route Table'):
-                route_table = True
-            if route_table:
-                if line.split()[0] == "0.0.0.0" and line.split()[1] == "0.0.0.0":
-                    return line.split()[2]
+    def stop(self):
+        route = Route()
+        route.restore_saved_default_gateway()
 
     def start(self):
-        getway = self.parse_getway()
-        subprocess.call(['route', 'delete', '0.0.0.0', 'mask', '0.0.0.0'])
-        return getway
+        route = Route()
+        route.save_default_gateway()
+        route.remove_default_getway()
