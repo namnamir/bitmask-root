4f2c7bf86fef1df6e5981ed20b3bd2a660e5f3db
Alireza <alireza@iransec.org>
2015-09-28 22:02:23 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 import py2exe
 import sys
 
-sys.argv.append("py2exe")
+sys.argv.append('py2exe')
 
 opts = {'py2exe': {
-    'dist_dir': "dist",
+    'dist_dir': 'dist',
     'dll_excludes': ['OLEAUT32.dll', 'USER32.dll', 'SHELL32.dll', 'ole32.dll', 'MSVCP90.dll',
                      'ADVAPI32.dll', 'NETAPI32.dll', 'WS2_32.dll', 'GDI32.dll', 'VERSION.dll', 'KERNEL32.dll'],
     'bundle_files': 2,
-    "optimize": 2,
+    'optimize': 2,
     'includes': ['zmq.backend.cython', 'UserList', 'UserString', 'commands', 'future.backports.misc'],
 }, }
 
