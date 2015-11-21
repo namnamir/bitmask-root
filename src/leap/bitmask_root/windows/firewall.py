65219232bf3ced0ec4ebf48a3d7929dc20c0f3a0
Alireza <alireza@iransec.org>
2015-11-21 13:28:18 +0330

    Update firewall.py

--- src/leap/bitmask_root/windows/firewall.py
+++ src/leap/bitmask_root/windows/firewall.py
@@ -1,4 +1,4 @@
-from src.leap.bitmask_root.windows.route import Route
+from src.leap.bitmask_root.windows.route_manager import RouteManager
 
 
 class Firewall:
@@ -6,10 +6,10 @@ class Firewall:
         pass
 
     def stop(self):
-        route = Route()
+        route = RouteManager()
         route.restore_saved_default_gateway()
 
     def start(self):
-        route = Route()
+        route = RouteManager()
         route.save_default_gateway()
         route.remove_default_getway()
