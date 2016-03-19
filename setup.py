f75fec3e87f478269a9adb5160769525f8d54efb
Alireza Mirzaeiyan <alireza.mirzaeiyan@yahoo.com>
2016-03-19 13:16:28 +0330

    Fix py2exe config

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
+import os
 from distutils.core import setup
 import py2exe
 import sys
+import zmq.libzmq
+from py2exe.build_exe import Target
 
 sys.argv.append('py2exe')
 
-opts = {'py2exe': {
-    'dist_dir': 'dist',
-    'dll_excludes': ['OLEAUT32.dll', 'USER32.dll', 'SHELL32.dll', 'ole32.dll', 'MSVCP90.dll',
-                     'ADVAPI32.dll', 'NETAPI32.dll', 'WS2_32.dll', 'GDI32.dll', 'VERSION.dll',
-                     'KERNEL32.dll', 'WINSPOOL.DRV', 'mfc90.dll'],
-    'includes': ['zmq.backend.cython', 'UserList', 'UserString', 'commands', 'future.backports.misc'],
-}, }
+service = Target(
+    description="Administrative service for bitmask client",
+    modules=["windows_installer"],
+    cmdline_style='pywin32',
+)
 
-setup(service=['windows_installer'], options=opts, cmdline_style='pywin32', zipfile=None)
+opts = {'py2exe': {
+    'dll_excludes': ['libzmq.pyd', 'OLEAUT32.dll', 'USER32.dll', 'SHELL32.dll', 'ole32.dll',
+                     'MSVCP90.dll', 'ADVAPI32.dll', 'NETAPI32.dll', 'WS2_32.dll', 'GDI32.dll',
+                     'VERSION.dll', 'KERNEL32.dll', 'WINSPOOL.DRV', 'mfc90.dll', 'ntdll.dll'],
+    'includes': ['UserList', 'UserString', 'commands', 'zmq.backend.cython'],
+    'dist_dir': "dist"
+}}
+
+setup(service=[service], options=opts, zipfile=None,
+      data_files=[(os.path.join(os.getcwd(), 'dist'), (zmq.libzmq.__file__,))])
